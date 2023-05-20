aeb7b28ba171ceb7432a5c369a3076a1fd3fb126
Alyona <alyonakisha1@rambler.ru>
2023-05-19 17:13:30 -0700

    Initial Commit

---
+++ CleanMVVM/CleanMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanMVVMApp.swift
+//  CleanMVVM
+//
+//  Created by MacBook Air on 19.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CleanMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
